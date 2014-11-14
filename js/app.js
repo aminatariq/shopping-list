85e307e1f1cd3b7fdde3359793ab7ef86163955b
aminatariq <aminatariq@gmail.com>
2014-11-13 23:00:34 -0500

    Edited some js

--- js/app.js
+++ js/app.js
@@ -1,40 +1,34 @@
 $(document).ready(function(){
 
-// use enter to add list items
+/* use enter to add list items */
     $('#item').keyup(function(event){
         if(event.keyCode == 13) {
             $('#add').click();
         };
     }); 
 
-// add list items
+/* add list items */
     $('#add').click(function(){
-        var txtbox = document.getElementById('item');
-        var txtval = txtbox.value;
-        event.preventDefault();
 
-        if(!$.trim($('#item').val())) {
-            alert('Please enter text to add to the list');
-        } else {
-            $('<li class="items"></li>').appendTo('#list').html('<div class="box"></div><span>' + txtval + '</span><img class="delete" src="img/delete.png"/>');
+     {
+        $('<li class="items"></li>').appendTo('#list').html('<div class="box"></div><span>' + txtval + '</span><img class="delete" src="img/delete.png"/>');
 
         document.getElementById('item').value = '';
         };
     });
 
-// delete items on list
+/* delete items on list */
     $('#list').on('click', '.delete', function(e){$(this).parent().remove()});
 
-// strike off items on list
+/* strike off items on list */
     $('#list').on('click', 'li', function(){$(this).toggleClass('strike'); $(this).children('.box').toggleClass('Checked');});
 
-// sorts items on list
+/* sorts items on list */
     $('#list').sortable({ axis: "y" });
 
 });
 
-// removes all items from list
+/* removes all items from list */
 $(document).on("click", "#startover", function(){
     $("#list").empty();
-    submissions = 0;
 });
